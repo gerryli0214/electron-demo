79e58f2e2e49292dfc20cdfde24a8db1ac703f5f
gerry0214 <957282514@qq.com>
2021-07-27 16:19:01 +0800

    修改单元测试配置

--- nyc.config.js
+++ nyc.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   all: true,
   include: [
-    "web/src/lib/**.js"
+    "web/src/lib/**.js",
+    ".electron/class/**.js"
   ],
   exclude: [
-    "web/test/*.spec.js"
+    "web/test/*.spec.js",
+    ".electron/test/*.spec.js"
   ],
   checkCoverage: false,
   reporter: ['text', 'html']
